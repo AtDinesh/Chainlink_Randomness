a04c1b28bb7e860db8997513878a6ea212acd9ef
Dinesh Atchuthan <a.dinesh@hotmail.fr>
2023-09-05 23:45:35 +0200

    start contract and define necessary variables

---
+++ hardhat/contracts/RandomWinnerGame.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
+
+contract RandomWinnerGame is VRFConsumerBase, Ownable {
+
+    /// variables
+    // Chainlink-related variable
+    // amount of LINK to send with request
+    uint256 public fee;
+    // ID of public key against which randomness is generated
+    bytes32 public keyHash;
+
+    // Addresses of the players in the game
+    address[] public players;
+    // Max number of players in the game
+    uint8 maxPlayers;
+    // has the game started ?
+    bool public gameStarted;
+    // fees for entering a game
+    uint256 entryFee;
+    // current game id
+    uint256 public gameId;
+
+    
+
+}
