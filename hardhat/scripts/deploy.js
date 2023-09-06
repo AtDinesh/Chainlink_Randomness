a25200811f777473473ddb98fb49c4797d61afaa
Dinesh Atchuthan <a.dinesh@hotmail.fr>
2023-09-06 23:43:20 +0200

    update deployment script

--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -4,30 +4,40 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
-const hre = require("hardhat");
+const {hre, ethers} = require("hardhat");
+require("dotenv").config({ path: ".env"});
+const { FEE, VRF_COORDINATOR, LINK_TOKEN, KEY_HASH } = require("../constants")
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
-
-  const lockedAmount = hre.ethers.parseEther("0.001");
-
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+  // deploy contract
+  const randomWinnerGame = await hre.ethers.deployContract(
+    "RandomWinnerGame",
+    [VRF_COORDINATOR, LINK_TOKEN, KEY_HASH, FEE]
   );
+  
+  await randomWinnerGame.waitForDeployment();
+  console.log("Contract deployed to:", randomWinnerGame.target);
+
+  // Wait for etherscan to catch up
+  console.log("Sleeping... wait for etherscan to catch up");
+  sleep(30 * 1000);
+
+  // verify the contract
+  await hre.run("verify:verify", {
+    address: randomWinnerGame.address,
+    constructorArguments: [VRF_COORDINATOR, LINK_TOKEN, KEY_HASH, FEE],
+  });
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
